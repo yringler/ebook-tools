528c8bb98a35a152afcf513e5613114fbfae48e6
Yehuda Ringler <guitarguy613@gmail.com>
2013-08-15 16:35:16 +0000

    rangeIterator:update

--- orayta/src/rangeIterator.h
+++ orayta/src/rangeIterator.h
@@ -1,35 +1,46 @@
 #ifndef RANGE_ITERATOR_H
 #define RANGE_ITERATOR_H
 
+#include <iterator>
+
 /* A wrapper around an iterator. operator++ skips over values within range */
 /* the beggining and end of range are acceptable values */
-template <typename ValueT, class IterT>	// type of the iterator and *iterator
-class RangeIterator : public IterT
+// ( design from std::reverse_iterator, as written in c++ in a nutshell )
+template <class Iterator>
+class RangeIterator : public std::iterator<
+	      typename std::iterator_traits<Iterator>::iterator_category,
+	      typename std::iterator_traits<Iterator>::value_type,
+	      typename std::iterator_traits<Iterator>::diffrence_type,
+	      typename std::iterator_traits<Iterator>::pointer,
+	      typename std::iterator_traits<Iterator>::refrence>
 {
 private:
-	IterT end;	// so doesn't skip past the end
+	typedef typename std::iterator_traits<Iterator>::value_type ValueT;
 
+	Iterator current;	// current location
+	Iterator end;	// so doesn't skip past the end
+
+	// range of values not to skip
 	ValueT lowest;
 	ValueT highest;
 public:
 	// the initial place to point to, the past-the-end iterator,
 	// and acceptable range of the values
-	RangeIterator(IterT & start, IterT & a_end, ValueT a, ValueT b)
-		: IterT(start)
-	{
+	RangeIterator(Iterator & a_current, Iterator & a_end,
+			ValueT a_lowest, ValueT a_highest) {
+		current = a_current;
 		end = a_end;
-		lowest = a, end = b;
+		lowest = a_lowest;
+		highest = a_highest;
 	}
-	RangeIterator() {}
 
-	void set(IterT start, IterT a_end) { operator=(start), end = a_end; }
-	virtual ValueT operator*() const { return IterT::operator*(); }
-	virtual void operator++() {
+	ValueT operator*() const { return *current; }
+	void operator++() {
 		while(*this <= lowest && *this >= highest &&  this != end) {
-			IterT::operator++();
+			current++;
 		}
 	}
-	virtual void operator++(int) { operator++(); }
+	void operator++(int) { operator++(); }
 };
 
 #endif
