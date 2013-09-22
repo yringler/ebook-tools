0da35d75af793e55d373121f73659dcdfbd15cc8
Yehuda Ringler <guitarguy613@gmail.com>
2013-09-22 03:11:08 -0400

    union.h:changed from struct to class
    otherwise, able to use as group of pointers, contrary to intent.

--- orayta/lib/union.h
+++ orayta/lib/union.h
@@ -1,32 +1,32 @@
 #ifndef UNION_H
 #define UNION_H
 
-
 #include <iostream>
 
 template<typename T1, typename T2>
-struct Union
+class Union
 {
-T1 *first;
-T2 *second;
-
 private:
+	T1 *m_first;
+	T2 *m_second;
 	short m_active;
 public:
-	Union() : m_active(-1) { first=second=0; }
+	Union() : m_active(-1) { m_first=m_second=0; }
 	void use(short i) {
 		switch(i)
 		{
-		case 1: delete second, second = 0;
-			first = new T1, m_active = 1;
+		case 1: delete m_second, m_second = 0;
+			m_first = new T1, m_active = 1;
 			break;
-		case 2: delete first, first = 0;
-			second = new T2, m_active = 2;
+		case 2: delete m_first, m_first = 0;
+			m_second = new T2, m_active = 2;
 			break;
 		default: std::cerr << "Union.use()::error\n"; throw;
 		}
 	}
 	short active() { return m_active; }
+	T1 & first() { return *m_first; }
+	T2 & second() { return *m_second; }
 };
 
 #endif
