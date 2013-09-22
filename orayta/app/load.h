0ff167d80e09f776f55013c36fa64582c0ad3102
Yehuda Ringler <guitarguy613@gmail.com>
2013-09-22 10:49:57 -0400

    added load.h

---
+++ orayta/app/load.h
@@ -0,0 +1,14 @@
+#ifndef LOAD_H
+#define LOAD_H
+
+#include <vector>
+
+// vector<> from: where on marker line to start extracting location information
+// 	from[0] for level 0, etc
+// LoadT: eg deque<string>, ie chapter
+template <typename LoadT, class WithFunc>
+bool load(std::vector<short> loadFrom, LoadT & data, std::wifstream &)
+{
+}
+
+#endif
