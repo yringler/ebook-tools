b261dc37305f4c2f2fc2aa52ce3bdb55a6c57a65
Yehuda Ringler <guitarguy613@gmail.com>
2013-09-23 13:01:09 -0400

    shrank header.h to typedefs.h

--- orayta/app/typedefs.h
+++ orayta/app/typedefs.h
@@ -1,20 +1,12 @@
 // misc. generic stuff
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
 
 #include <vector>
 #include <deque>
 #include <string>
 #include "../lib/looseBinder.h"
-
-// tells program where he is in the file, like a sign post
-// eg "now your at a level 3 labled 'homeward bound'"
-// levels start at 0 (book), and go up untill reach actuall content
-struct Location
-{
-	std::wstring lable;
-	short level;
-};
+#include "location.h"
 
 /*
  * Each union is either data or positional data
@@ -36,7 +28,5 @@ typedef Union<CommentaryBinder, Location> Joiner;
 typedef std::deque<SourceText> SourceTextQueue;
 typedef std::deque<Commentary> CommentaryQueue;
 typedef std::deque<Joiner> JoinerQueue;
-
-void connect(Joiner & joiner, Commentary & commentary);
 
 #endif
