75986525137e796b8130ab445fb1435974e04211
Yehuda Ringler <guitarguy613@gmail.com>
2013-09-23 12:54:18 -0400

    seperated joinTools from header.h

---
+++ orayta/app/joinTools.cpp
@@ -0,0 +1,35 @@
+#include <cassert>
+#include "joinTools.h"
+#include "location.h"
+
+void connect(JoinerQueue & joinerQueue, SourceTextQueue & sourceTextQueue)
+{
+	Joiner tmp;
+	tmp.use(1);
+
+	for(SourceTextQueue::iterator iter = sourceTextQueue.begin();
+			iter != sourceTextQueue.end(); iter++) {
+		if(iter->active() == 1) {
+			joinerQueue.push_back(*iter);
+		} else {
+			// set joiner to source text
+			tmp.first().to(iter->first());
+			joinerQueue.push_back(tmp);
+		}
+	}
+}
+
+void connect(JoinerQueue & joinerQueue, CommentaryQueue & commentaryQueue)
+{
+	JoinerQueue::iterator jit;
+	JoinerQueue::iterator jend = joinerQueue.end();
+	CommentaryQueue::iterator cit;
+	CommentaryQueue::iterator cend = CommentaryQueue.end();
+
+	while(moveToHighestLocationTogether(jit, jend, cit, cend)) {
+		jit++;
+		cit++;
+		// attach comment to source text
+		jit->first().attach(cit->first());
+	}
+}
---
+++ orayta/app/joinTools.h
@@ -0,0 +1,9 @@
+#ifndef JOIN_TOOLS_H
+#define JOIN_TOOLS_H
+
+#include "header.h"
+
+void connect(JoinerQueue &, SourceTextQueue &);
+void connect(JoinerQueue &, CommentaryQueue &);
+
+#endif
