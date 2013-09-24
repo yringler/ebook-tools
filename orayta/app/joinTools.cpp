3fb335758d358e19d4618cdd28a5b117ade9ea95
Yehuda Ringler <guitarguy613@gmail.com>
2013-09-24 18:36:25 -0400

    connect(joiner to source):use two tmp vars
    one for first() active, one for second()

--- orayta/app/joinTools.cpp
+++ orayta/app/joinTools.cpp
@@ -4,15 +4,18 @@
 
 void connect(JoinerQueue & joinerQueue, SourceTextQueue & sourceTextQueue)
 {
-	Joiner tmp;
-	tmp.use(1);
+	// where first is binder and second is location
+	Joiner first_active;
+	first_active.use(1);
+	Joiner second_active;
+	second_active.use(2);
 
 	for(SourceTextQueue::iterator iter = sourceTextQueue.begin();
 			iter != sourceTextQueue.end(); iter++) {
 		if(iter->active() == 1) {
 			// set joiner to source text
-			tmp.first().to(iter->first());
-			joinerQueue.push_back(tmp);
+			first_active.first().to(iter->first());
+			joinerQueue.push_back(first_active);
 		} else {
 			joinerQueue.push_back(*iter);
 		}
