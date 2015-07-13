cfb3356c2cfc83c483586334ea3dff80daffcea8
Yehuda Ringler <guitarguy613@gmail.com>
2015-07-13 15:33:56 -0400

    initial commit

--- orayta/app/locatedContentFileLoader.h
+++ orayta/app/locatedContentFileLoader.h
@@ -2,17 +2,54 @@
 #define LOCATED_CONTENT_FILE_LOADER_H
 
 #include <istream>
-#include "loadableLocatedContent"
+#include <deque>
+#include <string>
+#include <cassert>
+#include "locatedContent.h"
 
+// translates from marker to depth
+template <typename CharT>
+class MarkerDictionary
+{
+private:
+	std::basic_string<CharT> markers;
+public:
+	bool exists(CharT marker) { 
+		return markers.find(marker) != std::basic_string<CharT>::npos;
+	}
+	void add(CharT marker) {
+		if(!exists(marker))
+			markers += marker;
+	}
+	int depth(CharT marker) {
+		assert(exists(marker));
+		return markers.find(marker);
+	}
+};
+
+template<typename LocatedContentT, typename CharT>
+defaultLocatedContentLoader(const std::basic_string<CharT> & line,
+		LocatedContentT & locatedContent) {
+	locatedContent = line;
+}
 /*
  * Creates a deque of LocatedContent
- * To actually load creates an instance of BasicLoadableLocatedContent
+ * It loads two things:
+ * 	the location
+ * 	the content
+ * of an entire text file
+ *
+ * The loading of the data is done via template based functor ContentLoader()
  */
 
-// T will be a BasicLocatedContent specilization (I think thats the right word)
-template <typename CharT, T>
-class BasicLocatedContentFileLoader
+template <typename LocatedContentT, typename LocatedContentLoader =
+	defaultLocatedContentLoader<LocatedContentLoader, CharT>,
+	typename CharT>
+loadLocatedContentFile(const std::basic_istream<CharT> & stream,
+		const std::basic_string<CharT> & markers)
 {
-};
+	std::deque<LocatedContentT> locatedContentQueue;
+	MarkerDictionary<CharT> markerDictionary;
+}
 
 #endif
---
+++ orayta/src/commentGroup.h
@@ -0,0 +1,26 @@
+#ifndef COMMENT_GROUP_H
+#define COMMENT_GROUP_H
+
+#include <deque>
+
+/*
+ * I think this is a good idea... I really shouldn't program when I'm tired.
+ * It makes a few things more conveniant:
+ * 	locating. All comments really are on the same location 
+ * 	loading. A single line in source could have a ton of comments, this way
+ * 		I can pass them all as a single argument.
+ * 	output. Probably. But I'm not thinking about that right now.
+ * 	It'll definitely make it easier to keep track of the silly things,
+ * 		which will make all dealings with them a bit easier
+ * 	(Although I'll confess that it does feel a bit silly to make an empty
+ * 	class like this.)
+ */
+template <typename CharT>
+class BasicCommentGroup : public std::deque<Comment<CharT> >
+{
+};
+
+typedef BasicCommentGroup<char> CommentGroup;
+typedef BasicCommentGroup<wchar_t> WCommentGroup;
+
+#endif
