0046cbbee456595f98539bc9ec06f632a6a4ab4e
Yehuda Ringler <guitarguy613@gmail.com>
2015-07-20 17:29:57 -0400

    gracefully handle all kinds of comments.
    spectacular. beutiful. inspired. words escape me. How could I have
    created something so beutifull? (kazakstan)

--- orayta/app/loadContent.h
+++ orayta/app/loadContent.h
@@ -2,7 +2,20 @@
 #define LOAD_CONTENT_H
 
 #include <string>
+#include <locale>
 #include "../lib/commentGroup.h"
+
+/*
+	Something useful. Two functions in this header are easier to understand
+	with this introduction.
+
+	q < b >
+	0 1 2 3
+	index of quote start marker == 1, length == 3
+	there are 4 charachters which need to be erased
+	index of start of marker(1) + length of marker(3)
+*/
+
 
 template<typename ContentT>
 void loadContent(ContentT & content, const std::wstring & line) {
@@ -11,57 +24,88 @@ void loadContent(ContentT & content, const std::wstring & line) {
 
 // Commentary is loaded *way* diffrent than other stuff
 // This is *so* ingenuis. (I hope I spelled that right)
+template<typename CommentT, CharT>
+void loadContent(CommentGroup<CommentT> & content, const std::wstring & line
+	// eg <b>quote1</b>commentcomment<b>quote2</b>commentcomment etc
+	// <b> marks the start of the quote, </b> marks the end
+		const std::basic_string<CharT> & quote_start_marker,
+		const std::basic_string<CharT> & quote_end_marker)
+{
+	CommentT tmp;
+	std::wstring::size_type i = line.find(quote_start_marker);
+	
+	// if theres an introductory part that isn't on a particular quote
+	if(i > 10) {
+		tmp.comment = line.substr(i);
+		content.push_back(tmp);
+	}
+
+	while(i != std::wstring::npos) {
+		// erase untill start of quote
+		line.erase(0, i+quote_start_marker.length();
+
+		i = line.find(quote_end_marker);
+		tmp.on = line.substr(0, i);	// extract quote
+
+		// erase until start of comment
+		line.erase(0, i + quote_end_marker.length());
+
+		// extract comment
+		// if not followed by comment, find returns npos, which causes
+		// substr to use until end of string. nice
+		i = line.find(quote_start_marker);
+		tmp.comment = line.substr(0, i);
+
+		content.push_back(tmp);
+	}
+}
+
+template <typename CharT>
+std::basic_string<CharT> & trimSpace(std::basic_string<CharT> & str) {
+	while(std::isspace(str[0], std::locale()))
+		str.erase(0,1);
+	while(std::isspace(str[str.length()-1], std::locale()))
+		str.erase(str.length()-1, 1);
+	return str;
+}
+
 template<typename CommentT>
 void loadContent(CommentGroup<CommentT> & content, const std::wstring & line)
 {
-	// eg <b>quote1</b>commentcomment<b>quote2</b>commentcomment etc
-	// quotes 1 and 2 are the quotes from source text that are being
-	// commented on
-	// and <b> and </b> are the markers, or marks
-	// I don't use <b> etc because sometimes its <B>
-	
-	// start of chars which mark that a quote is coming
-	std::wstring quote_mark_start(L"<");	
+	// the seif katan for mishna berura comes between these two things
+	const std::wstring mb_sk_start(L"<span class=\"S0\">")
+	const std::wstring mb_sk_end(L"</span>");
 
-	std::wstring before_quote_start(L">");
-	std::wstring after_quote_end(L"</");	
-	
-	std::wstring before_comment_start(L">")
+	if(line.find(L"<b>") != std::wstring::npos)
+		loadContent(content, line, L"<b>", L"</b>");
+	else if(line.find(L"<B>") != std::wstring::npos)
+		loadContent(content, line, L"<B>", L"</B>");
+	else if(line.find(L"{") != std::wstring::npos)
+		loadContent(content, line, L"{", L"}");
+	else if(line.find(L"-") != std::wstring::npos) {
+		// mishna berura has the honour of being processed right here
+		// the length of the substring up to and including "-" is its
+		// index + 1 (see comment before previous function)
+		// But I don't want "-", so I don't add 1
 
-	WComment tmp;
-	std::wstring::size_type i = line.find(before_quote_start);
-	
-	// if theres an introductory part, not on a particular quote
-	if(i > 10) {
-		tmp.comment = line.substr(line.find(quote_mark_start));
+		content.back().on = trimSpace(line.substr(0, line.find(L"-")));
+		line.erase(line.find(L"-"));
+		content.back().comment += line;	// append, after seif katan
+	} else if(line.find(mb_sk_start) != std::wstring::npos) {
+		// mishna berura again. This line contains seif katan.
+		// I might handle that later, or I might prefer to convert it
+		// from the number. Tz"I
+		// 
+		// For now I'll ignore it, no great shakes, because the
+		// shulchon aruch text doesn't use the seif katan system
+		// actually - I guess for now I could just stick it on to the
+		// comment
+
+		line.erase(0, line.find(mb_sk_start) + mb_sk_start.length());
+		ContentT tmp;
+		tmp.comment = line.substr(0, line.find(mb_sk_end));
 		content.push_back(tmp);
 	}
-
-	while( (i = line.find(before_quote_start)) != std::wstring::npos) {
-		// +1 because index starts at 0, and length starts at 1
-		line.erase(0, i+1);
-
-		// not +1, because I don't want that '<' also
-		tmp.on = line.substr(0, line.find(after_quote_end);	
-
-		line.erase(0, line.find(before_comment_start) + 1);
-
-		// if not followed by comment, find returns npos, which causes
-		// substr to use untill end of string. nice
-		tmp.comment = line.substr(0, line.find(quote_mark_start));
-
-		content.push_back(tmp);
-	}
-}
-
-template<typename CommentT>
-void loadMishnaBerura(CommentGroup<CommentT> & content)
-{
-}
-
-template<typename CommentT>
-void loadIkkurTosfosYomtov(CommentGroup<CommentT> & content)
-{
 }
 
 #endif
